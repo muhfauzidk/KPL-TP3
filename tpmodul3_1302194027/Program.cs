6c737c66e9e3f384b8279e0abb2b22651c2cb3d1
Muhammad Fauzi Dwikurnia <muhfauzidk@gmail.com>
2022-03-11 14:51:57 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul3_1302194027/Program.cs
+++ tpmodul3_1302194027/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Kodepos
+{
+    public static void Main(string[] args)
+    {
+
+        static int getKodePos(int kodePos)
+        {
+            int[] KP = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286
+                     , 40272, 40274, 40273 };
+
+            string[] kel = { "Batununggal", "Kujangsari", "Mengger", "Wates", "Cijaura",
+                         "Jatisari", "Margasari", "Sekejati", "Kebonwaru", "Maleer", "Samoja" };
+
+            return KP[kodePos];
+
+        }
+        int i = 0;
+        while(i < 11)
+        {
+            Console.WriteLine(getKodePos(i));
+            i++;
+        }
+
+        
+    }
+}
